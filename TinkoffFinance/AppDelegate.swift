b649fcceafc76c5a0a855154dd075e2988552d34
web-gardener <zhulishen0331@gmail.com>
2018-10-01 20:41:38 +0300

    fix: fixed some stuff before HW2

--- TinkoffFinance/AppDelegate.swift
+++ TinkoffFinance/AppDelegate.swift
@@ -45,16 +45,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        #if DEBUG
         stateChangeLogger(from: "Not running") // переходим из "not running" -> "inactive"
-        #endif
         return true
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        #if DEBUG
         // Здесь по прежнему остаемся Inactive....
+        #if DEBUG
         if application.applicationState == .inactive {
             print("Current state is Inactive... \(#function)")
             print("********************************************\n")
@@ -66,39 +64,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        #if DEBUG
         stateChangeLogger(to: "Inactive") // переходим из "Active" -> "inactive"
-        #endif
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        #if DEBUG
         stateChangeLogger(from: "Inactive")
-        #endif
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        #if DEBUG
-        stateChangeLogger(to: "Inactive") 
-        #endif
+        stateChangeLogger(to: "Inactive")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        #if DEBUG
         stateChangeLogger(from: "Inactive") // переходим из "inactive" -> "Active"
-        #endif
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        #if DEBUG
         stateChangeLogger(to: "Not running") // переходим из "not running" -> "inactive"
-        #endif
         self.saveContext()
     }
 
