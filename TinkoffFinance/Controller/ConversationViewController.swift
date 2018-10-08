52069ef76c90764473cf1ac5dab00a6d78c22a02
web-gardener <zhulishen0331@gmail.com>
2018-10-08 03:24:11 +0300

    feature: added some design features

--- TinkoffFinance/Controller/ConversationViewController.swift
+++ TinkoffFinance/Controller/ConversationViewController.swift
@@ -9,22 +9,16 @@
 import UIKit
 
 class ConversationViewController: UIViewController {
-
+    /* OUTLETS */
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       /* Метод для менюшки сбоку
+          
+         */ menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
